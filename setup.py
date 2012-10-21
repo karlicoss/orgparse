88b7f7bf0ac8891efe005b1b9deb1edb3a8aec90
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-21 20:04:53 +0200

    Add more classifiers to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,13 @@ setup(
     keywords='org-mode, Emacs, parser',
     classifiers=[
         "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
 )
