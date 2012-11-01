2086b89021faf547b597d7e18d1b83cb6f298730
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-01 11:40:22 +0100

    Fix tests/data/03_repeated_tasks.py

--- orgparse/tests/data/03_repeated_tasks.py
+++ orgparse/tests/data/03_repeated_tasks.py
@@ -6,8 +6,8 @@ data = [dict(
     todo='TODO',
     deadline=OrgDateDeadline((2005, 10, 1)),
     repeated_tasks=[
-        OrgDateRepeatedTask((2005, 9, 1, 16, 10, 0), 'DONE', 'TODO'),
-        OrgDateRepeatedTask((2005, 8, 1, 19, 44, 0), 'DONE', 'TODO'),
-        OrgDateRepeatedTask((2005, 7, 1, 17, 27, 0), 'DONE', 'TODO'),
+        OrgDateRepeatedTask((2005, 9, 1, 16, 10, 0), 'TODO', 'DONE'),
+        OrgDateRepeatedTask((2005, 8, 1, 19, 44, 0), 'TODO', 'DONE'),
+        OrgDateRepeatedTask((2005, 7, 1, 17, 27, 0), 'TODO', 'DONE'),
     ]
 )]
