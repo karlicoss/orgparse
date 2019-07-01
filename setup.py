3ae9503b5ee58c692177edf3f71610585ab6987e
Dima Gerasimov <karlicoss@gmail.com>
2019-07-01 19:54:25 +0100

    Prepare for 0.1 release

--- setup.py
+++ setup.py
@@ -22,14 +22,17 @@ setup(
     long_description=orgparse.__doc__,
     keywords='org-mode, Emacs, parser',
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        'Development Status :: 5 - Production/Stable'
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Markup',
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
 )
