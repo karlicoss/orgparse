ee5dcba60e728743ac1fb4e5f0013b17fc4d9d9c
Dima Gerasimov <karlicoss@gmail.com>
2020-11-01 15:46:31 +0000

    drop py2/py3.5 support
    I'd prefer not too, but IMO typing module/mypy annotations are too valuable

--- setup.py
+++ setup.py
@@ -39,14 +39,6 @@ def main():
         classifiers=[
             'Development Status :: 5 - Production/Stable',
             'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
             'Topic :: Text Processing :: Markup',
             # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         ],
