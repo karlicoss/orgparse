karlicoss/orgparse

commit 30671890e38db0c7becab12dfda6ff7d97496a8b

add .body property for root node

orgparse/__init__.py
@@ -127,6 +127,7 @@ def load(path, env=None):
     :rtype: :class:`orgparse.node.OrgRootNode`
 
     """
+    path = str(path) # in case of pathlib.Path
     if isinstance(path, basestring):
         orgfile = codecs.open(path, encoding='utf8')
         filename = path

orgparse/tests/test_misc.py
@@ -14,6 +14,18 @@ def test_empty_heading() -> None:
     assert h.tags == {'sometag'}
 
 
+def test_root() -> None:
+    root = loads('''
+#+STARTUP: hidestars
+Whatever
+# comment
+* heading 1
+    '''.strip())
+    assert len(root.children) == 1
+    # todo not sure if should strip special comments??
+    assert root.body.endswith('Whatever\n# comment')
+
+
 def test_stars():
     # https://github.com/karlicoss/orgparse/issues/7#issuecomment-533732660
     root = loads("""

orgparse/node.py
@@ -353,6 +353,8 @@ class OrgBaseNode(Sequence):
     5
     """
 
+    _body_lines: List[str] # set by the child classes
+
     def __init__(self, env, index=None) -> None:
         """
         Create an :class:`OrgBaseNode` object.
@@ -703,6 +705,30 @@ class OrgBaseNode(Sequence):
         """
         return self._get_tags(inher=False)
 
+    @staticmethod
+    def _get_text(text, format='plain'):
+        if format == 'plain':
+            return to_plain_text(text)
+        elif format == 'raw':
+            return text
+        else:
+            raise ValueError('format={0} is not supported.'.format(format))
+
+    def get_body(self, format='plain') -> str:
+        """
+        Return a string of body text.
+
+        See also: :meth:`get_heading`.
+
+        """
+        return self._get_text(
+            '\n'.join(self._body_lines), format) if self._lines else ''
+
+    @property
+    def body(self) -> str:
+        """Alias of ``.get_body(format='plain')``."""
+        return self.get_body()
+
     def is_root(self):
         """
         Return ``True`` when it is a root node.
@@ -757,7 +783,11 @@ class OrgRootNode(OrgBaseNode):
 
     """
 
-    # getter
+    @property
+    def _body_lines(self) -> List[str]: # type: ignore[override]
+        # todo hacky..
+        # for root node, the body is whatever is before the first node
+        return self._lines
 
     @property
     def level(self):
@@ -765,8 +795,6 @@ class OrgRootNode(OrgBaseNode):
 
     def get_parent(self, max_level=None):
         return None
-
-    # misc
 
     def is_root(self):
         return True
@@ -909,17 +937,6 @@ class OrgNode(OrgBaseNode):
         \[ (?P<date> [^\]]+) \]''',
         re.VERBOSE)
 
-    # getter
-
-    @staticmethod
-    def _get_text(text, format='plain'):
-        if format == 'plain':
-            return to_plain_text(text)
-        elif format == 'raw':
-            return text
-        else:
-            raise ValueError('format={0} is not supported.'.format(format))
-
     def get_heading(self, format='plain'):
         """
         Return a string of head text without tags and TODO keywords.
@@ -942,25 +959,10 @@ class OrgNode(OrgBaseNode):
         """
         return self._get_text(self._heading, format)
 
-    def get_body(self, format='plain'):
-        """
-        Return a string of body text.
-
-        See also: :meth:`get_heading`.
-
-        """
-        return self._get_text(
-            '\n'.join(self._body_lines), format) if self._lines else ''
-
     @property
-    def heading(self):
+    def heading(self) -> str:
         """Alias of ``.get_heading(format='plain')``."""
         return self.get_heading()
-
-    @property
-    def body(self):
-        """Alias of ``.get_body(format='plain')``."""
-        return self.get_body()
 
     @property
     def level(self):
@@ -1022,7 +1024,7 @@ class OrgNode(OrgBaseNode):
         """
         return self._todo
 
-    def get_property(self, key, val=None):
+    def get_property(self, key, val=None) -> Optional[PropertyValue]:
         """
         Return property named ``key`` if exists or ``val`` otherwise.
 
@@ -1036,7 +1038,7 @@ class OrgNode(OrgBaseNode):
         return self._properties.get(key, val)
 
     @property
-    def properties(self):
+    def properties(self) -> Dict[str, PropertyValue]:
         """
         Node properties as a dictionary.
 

commit 29058d9f9cb6ab196712e559867b68cc2c60e5d7

fix regression in orgparse.load method for file-like objects
broken in https://github.com/karlicoss/orgparse/commit/30671890e38db0c7becab12dfda6ff7d97496a8b

see https://github.com/karlicoss/orgparse/issues/32

orgparse/__init__.py
@@ -107,9 +107,11 @@ True
 # [[[end]]]
 
 import codecs
-from typing import Iterable
+from pathlib import Path
+from typing import Iterable, Union, Optional, TextIO
 
-from .node import parse_lines, OrgNode # todo basenode??
+
+from .node import parse_lines, OrgEnv, OrgNode # todo basenode??
 from .utils.py3compat import basestring
 
 __author__ = 'Takafumi Arakaki, Dmitrii Gerasimov'
@@ -117,7 +119,7 @@ __license__ = 'BSD License'
 __all__ = ["load", "loads", "loadi"]
 
 
-def load(path, env=None):
+def load(path: Union[str, Path, TextIO], env: Optional[OrgEnv]=None) -> OrgNode:
     """
     Load org-mode document from a file.
 
@@ -127,10 +129,10 @@ def load(path, env=None):
     :rtype: :class:`orgparse.node.OrgRootNode`
 
     """
-    path = str(path) # in case of pathlib.Path
-    if isinstance(path, basestring):
-        orgfile = codecs.open(path, encoding='utf8')
-        filename = path
+    orgfile: TextIO
+    if isinstance(path, (str, Path)):
+        orgfile = codecs.open(str(path), encoding='utf8')
+        filename = str(path)
     else:
         orgfile = path
         filename = path.name if hasattr(path, 'name') else '<file-like>'
@@ -138,7 +140,7 @@ def load(path, env=None):
                  filename=filename, env=env)
 
 
-def loads(string: str, filename='<string>', env=None) -> OrgNode:
+def loads(string: str, filename: str='<string>', env: Optional[OrgEnv]=None) -> OrgNode:
     """
     Load org-mode document from a string.
 
@@ -148,7 +150,7 @@ def loads(string: str, filename='<string>', env=None) -> OrgNode:
     return loadi(string.splitlines(), filename=filename, env=env)
 
 
-def loadi(lines: Iterable[str], filename='<lines>', env=None) -> OrgNode:
+def loadi(lines: Iterable[str], filename: str='<lines>', env: Optional[OrgEnv]=None) -> OrgNode:
     """
     Load org-mode document from an iterative object.
 

orgparse/tests/test_misc.py
@@ -158,3 +158,14 @@ def test_filetags_are_tags() -> None:
     assert root.tags == {'f1', 'f2'}
     child = root.children[0].children[0]
     assert child.tags == {'f1', 'f2', 'h1'}
+
+
+def test_load_filelike() -> None:
+    import io
+    stream = io.StringIO('''
+* heading1
+* heading 2
+''')
+    root = load(stream)
+    assert len(root.children) == 2
+    assert root.env.filename == '<file-like>'